adbdfc3d168f3de61a0aeae17cc06889920fff78
ekincimiran17-arch <ekincimiran17@gmail.com>
2026-03-22 13:12:30 +0300

    Update index.js with new bot code

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.get('/', (req, res) => {
     res.send('WhatsApp Group Auto Message Bot is running!');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
